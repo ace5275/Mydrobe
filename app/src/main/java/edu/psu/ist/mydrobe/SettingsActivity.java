bc8b28c165d56a050bf14479ef038196ffd83a05
Aidan Engleka <ace5275@psu.edu>
2022-04-25 23:38:38 -0400

    added layouts and code for dark mode, not functional but it runs

--- app/src/main/java/edu/psu/ist/mydrobe/SettingsActivity.java
+++ app/src/main/java/edu/psu/ist/mydrobe/SettingsActivity.java
@@ -1,15 +1,27 @@
 package edu.psu.ist.mydrobe;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.google.android.material.imageview.ShapeableImageView;
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
+
+    public static final String TAG = "SETTINGS";
+
+    public static final String EXTRA_DARK_MODE = "RETURN_DARK_MODE_STATE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +39,48 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         if (eventSourceId == R.id.settings_back_button) {
             handleBackButtonClick();
+        } else if (eventSourceId == R.id.applyButton) {
+            handleApplyButtonClick();
+        }  else if (eventSourceId == R.id.cancelButton) {
+            handleBackButtonClick();
         }
     }
 
     public void handleBackButtonClick(){
         Intent goBack = new Intent(this, MainActivity.class);
         startActivity(goBack);
+    }
+
+    public void handleApplyButtonClick(){
+        Intent apply = new Intent(this, MainActivity.class);
+
+        mGetStatus.launch(apply);
+    }
+
+    ActivityResultLauncher<Intent> mGetStatus = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                int resultCode = result.getResultCode();
+                if (resultCode == RESULT_OK) {
+                    assert result.getData() != null;
+                    boolean darkModeStatus = result.getData().getBooleanExtra(EXTRA_DARK_MODE, false);
+
+                    SwitchCompat darkModeSwitch = findViewById(R.id.darkModeSwitch);
+                    if (darkModeSwitch.isChecked() != darkModeStatus) {
+                        onCheckedChanged(darkModeSwitch, darkModeStatus);
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    Log.d(TAG, "Canceled from ShowProfileDetailsActivity");
+                } else {
+                    Log.d(TAG, String.format("Unknown return code from ShowProfileDetailsActivity: %s", resultCode));
+                }
+            }
+    );
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(MainActivity.RESULT_SETTINGS_CHANGE_THEME, compoundButton.isChecked());
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 }
