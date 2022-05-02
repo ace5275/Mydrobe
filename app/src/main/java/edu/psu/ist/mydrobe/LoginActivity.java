7c49268a01c0eeb37401e25a91bdd573d99b68b9
Aidan Engleka <ace5275@psu.edu>
2022-05-02 16:52:36 -0400

    dark mode working in login

--- app/src/main/java/edu/psu/ist/mydrobe/LoginActivity.java
+++ app/src/main/java/edu/psu/ist/mydrobe/LoginActivity.java
@@ -4,13 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginActivity extends AppCompatActivity {
-
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +17,40 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button button = findViewById(R.id.login_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainActivity();
+        button.setOnClickListener(this);
 
-            }
+        Intent intent = getIntent();
+        int color = intent.getIntExtra(MainActivity.EXTRA_BACKGROUND_COLOR, 0);
+        setBackgroundColor(color);
 
-        });
+        TextView name = findViewById(R.id.name);
+        TextView pass = findViewById(R.id.textpassword);
+
+        if (color == R.color.highlight_blue){
+            button.setBackgroundColor(getColor(R.color.light_blue));
+            button.setTextColor(getColor(R.color.highlight_blue));
+
+            pass.setTextColor(getColor(R.color.light_blue));
+            name.setTextColor(getColor(R.color.light_blue));
+        } else {
+            name.setTextColor(getColor(R.color.highlight_blue));
+            pass.setTextColor(getColor(R.color.highlight_blue));
+
+            button.setBackgroundColor(getColor(R.color.highlight_blue));
+            button.setTextColor(getColor(R.color.white));
+        }
 
     }
-    public void openMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login_button){
+            finish();
+        }
+
+    }
+
+    private void setBackgroundColor(int color) {
+        findViewById(R.id.add_clothing_activity).setBackgroundColor(getColor(color));
     }
 }
